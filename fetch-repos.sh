b5b6880fd59f2ef7043ce793e88b3784d80895f7
dreamshader <dreamshader@gmx.de>
2015-10-11 06:34:46 +0200

    initial checkin

---
+++ fetch-repos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+mkdir ROBOT
+cd ROBOT
+git init
+git remote add origin https://github.com/dreamshader/Robot
+git pull origin master
+git config --global user.email "dreamshader@gmx.de"
+git config --global user.name "dreamshader"
+cd ..
+#
+mkdir ARDUINO
+cd ARDUINO
+git init
+git remote add origin https://github.com/dreamshader/Arduino
+git pull origin master
+git config --global user.email "dreamshader@gmx.de"
+git config --global user.name "dreamshader"
+cd ..
+#
+mkdir LINUX
+cd LINUX
+git init
+git remote add origin https://github.com/dreamshader/Linux
+git pull origin master
+git config --global user.email "dreamshader@gmx.de"
+git config --global user.name "dreamshader"
+cd ..
+#
+mkdir RASPBERRY_PI
+cd RASPBERRY_PI
+git init
+git remote add origin https://github.com/dreamshader/Raspberry_PI
+git pull origin master
+git config --global user.email "dreamshader@gmx.de"
+git config --global user.name "dreamshader"
+cd ..
+#
+
